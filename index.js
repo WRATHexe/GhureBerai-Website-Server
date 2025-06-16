afad9ea2de9a214cd4565dcc04a92f2455bcc814
WRATHeXe <FardinRuhian001@gmail.com>
2025-06-17 04:10:09 +0600

    Update MongoDB connection string to use environment variables for credentials

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const cors= require('cors');
 const app = express();
 const port =  process.env.PORT3000 || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
 
 
 
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.esy9tcr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.esy9tcr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+console.log({ DB_USER: process.env.DB_USER, DB_PASS: process.env.DB_PASS });
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,6 +27,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
