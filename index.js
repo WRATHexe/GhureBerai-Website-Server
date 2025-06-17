bf5a9ead3978d04ec0348cbfd86680df77df13ba
WRATHeXe <FardinRuhian001@gmail.com>
2025-06-17 07:04:02 +0600

    Fix port configuration and add tour package routes and used MongoDB’s $regex operator to match tour names or destinations

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const cors= require('cors');
 const app = express();
-const port =  process.env.PORT3000 || 3000;
+const port =  process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 app.use(cors());
@@ -11,8 +11,6 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.esy9tcr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-console.log({ DB_USER: process.env.DB_USER, DB_PASS: process.env.DB_PASS });
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,8 +26,42 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // Database and collection setup
+    const collection = client.db("TourBuzzDB").collection("tourPackagesCollection");
+
+    app.post('/tourPackages', async (req, res) => {
+        try {
+            const tourPackage = req.body;
+            const result = await collection.insertOne(tourPackage);
+            res.send(result);
+        } catch (error) {
+            res.status(500).send({ error: 'Failed to add tour package' });
+        }
+    });
+
+    app.get('/tourPackages', async (req, res) => {
+      const { search } = req.query;
+      let query = {};
+      if (search) {
+      query = {
+        $or: [
+        { tourName: { $regex: search, $options: "i" } },
+        { destination: { $regex: search, $options: "i" } }
+        ]
+      };
+      }
+      try {
+      const packages = await collection.find(query).toArray();
+      res.json(packages);
+      } catch (error) {
+      res.status(500).send({ error: 'Failed to fetch tour packages' });
+      }
+    });
+
 
     
+
+
 
 
 
